689851b8c076f3be7f4966d6a45aeda03c3f8a14
omeagazord <omeagazord@omeagazord-HP-ProBook-450-G3>
2017-08-17 15:39:45 +0200

    Gestion click
    test pour la gestion du click

--- process.php
+++ process.php
@@ -1,6 +1,25 @@
 <?php
 
 
+$fctn = $_POST['fonction'];
+$fctn();
 
+function testClickDossier()
+{
+    //echo "L'id a été traité par le serveur: " . $_POST['dossier'];
+    
+    $path = "ls /home/" . $_POST['dossier'];
+    
+    $output = shell_exec($path);
+    $resultat = preg_split('/\s+/', $output);
+    
+    for($compteur=0; $compteur< count($resultat); $compteur++)
+    {
+            echo '<div class="justify-content-center align-items-end">
+                    <div class="row col-4 offset-4 fondBleu" onclick="testalert()">
+                <i class="fa fa-folder-open fa-3x"></i><p class="tabulation">'
+                . $resultat[$compteur] . "</p></div></div>";
+    }
+}
 
 ?>
--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-
+function clickDossier(id)
+{
+	//alert(id);
+    $.ajax({
+        url:"process.php",
+        type: "post",
+        data: {fonction: "testClickDossier", dossier: id},
+        success:function(result){
+        	//alert(result);
+        	$('#testClickDossier').html(result);
+        }
+     });
+}
 
