817fb0bca2e6616348d64d37b521379097a2a26d
boul <boul@boul-HP-ProBook-450-G3>
2017-08-17 13:31:00 +0200

    squelette

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
-<?php
+<!doctype html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Explorateur de fichier</title>
+        <script type="text/javascript" src="libraries/tether-1.3.3/dist/js/tether.js"></script>
+        <script type="text/javascript" src="libraries/jquery-3.2.1.js"></script>
+        <script type="text/javascript" src="script.js"></script>
+        <link rel="stylesheet" href="libraries/bootstrap/css/bootstrap.css"> 
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+    <body class="container">
+        <h1>Votre explorateur de fichiers</h1>
+        <div>
+            <!-- Champs vides pour rentrer un répertoire cible,
+            Champs indiquant le repertoire courant-->
+        </div>
+        <div>
+            <!--liste des fichiers/dossiers-->
+        </div>
+        <div>
+            <!--boutton retour-->
+        </div>
+    </body>
+</html>
 
