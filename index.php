bb2534f4e968adea2d12af32c86dd3ebe2e3e169
omeagazord <omeagazord@omeagazord-HP-ProBook-450-G3>
2017-08-17 13:53:28 +0200

    font-awesome, organisation
    ajout de font-awesome et organisation du head de index.php

--- index.php
+++ index.php
@@ -4,12 +4,21 @@
     <head>
         <meta charset="utf-8">
         <title>Explorateur de fichier</title>
-        <script type="text/javascript" src="libraries/tether-1.3.3/dist/js/tether.js"></script>
-        <script type="text/javascript" src="libraries/jquery-3.2.1.js"></script>
-        <script type="text/javascript" src="script.js"></script>
-        <link rel="stylesheet" href="libraries/bootstrap/css/bootstrap.css"> 
-        <link rel="stylesheet" href="style.css">
         
+        <!-- libaries css-->
+        <link type="text/css" rel="stylesheet" href="libraries/bootstrap/css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="libraries/font-awesome-4.7.0/css/font-awesome.css">
+        
+        <!-- libraries js -->  
+        <script type="text/javascript" src="libraries/tether-1.3.3/dist/js/tether.js"></script>
+        <script type="text/javascript" src="libraries/jquery-3.2.1.js"></script>     
+        <script type="text/javascript" src="libraries/bootstrap/js/bootstrap.js"></script> 
+        
+         <!-- custom css  -->
+         <link rel="stylesheet" href="style.css">
+         
+         <!-- custom js  -->
+        <script type="text/javascript" src="script.js"></script>
     </head>
     <body class="container">
         <h1>Votre explorateur de fichiers</h1>
