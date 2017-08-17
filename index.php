f7137c269725b1c3957f5b35127af145ae0f4cef
boul <boul@boul-HP-ProBook-450-G3>
2017-08-17 15:34:53 +0200

    affichage des dossiers en dur

--- index.php
+++ index.php
@@ -4,32 +4,36 @@
     <head>
         <meta charset="utf-8">
         <title>Explorateur de fichier</title>
-        
+
         <!-- libaries css-->
         <link type="text/css" rel="stylesheet" href="libraries/bootstrap/css/bootstrap.css">
         <link type="text/css" rel="stylesheet" href="libraries/font-awesome-4.7.0/css/font-awesome.css">
-        
+
         <!-- libraries js -->  
         <script type="text/javascript" src="libraries/tether-1.3.3/dist/js/tether.js"></script>
         <script type="text/javascript" src="libraries/jquery-3.2.1.js"></script>     
         <script type="text/javascript" src="libraries/bootstrap/js/bootstrap.js"></script> 
-        
-         <!-- custom css  -->
-         <link rel="stylesheet" href="style.css">
-         
-         <!-- custom js  -->
+
+        <!-- custom css  -->
+        <link rel="stylesheet" href="style.css">
+
+        <!-- custom js  -->
         <script type="text/javascript" src="script.js"></script>
     </head>
-    <body class="container">
-        <h1>Votre explorateur de fichiers</h1>
-        <div class="">
-            <input method="post" type="text" name="position" placeholder="/home/boul" class="offset-2 col-8 offset-2">
-        </div><br>
-        <div id="dossier">
-            <!--liste des fichiers/dossiers-->
-        </div>
-        <div>
-            <!--boutton retour-->
+    <body>
+        <header>
+            <h1>Votre explorateur de fichiers</h1>
+        </header>
+        <div class="container">
+            <div class="row">
+                <input method="post" type="text" name="position" placeholder="/home/boul" class="offset-md-1 col-md-8">
+            </div><br>
+            <div id="dossier">
+                <!--liste des fichiers/dossiers-->
+            </div>
+            <div>
+                <!--boutton retour-->
+            </div>
         </div>
     </body>
 </html>
--- process.php
+++ process.php
@@ -7,8 +7,8 @@ function dossier() {
     $liste_dossier = shell_exec('ls /home/boul');
     $tabl_dossier = preg_split('/\s+/', $liste_dossier);
 
-    for ($i=0 ; $i < count($tabl_dossier) ; $i++) {
-        echo "<p class='folder'>${tabl_dossier[$i]}</p>";
+    for ($i=0 ; $i < count($tabl_dossier)-1 ; $i++) {
+        echo "<div class='folder ligne'><i class='fa fa-2x fa-folder-o'></i><p>${tabl_dossier[$i]}</p></div>";
     }
 }
 
