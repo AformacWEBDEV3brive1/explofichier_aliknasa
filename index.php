e32e8888eea303621e11874fda0e03112bd650ab
omeagazord <omeagazord@omeagazord-HP-ProBook-450-G3>
2017-08-18 08:36:50 +0200

    click
    mise en place du click des dossiers de home/user

--- index.php
+++ index.php
@@ -31,15 +31,6 @@
         	<div id="dossier" class="row">
             	<!--liste des fichiers/dossiers-->
         	</div>
-       		 <div id="testClickDossier">
-        		<!-- TEST CLICK DOSSIER -->
-        		<div id="dos1" class="row bgBleu" onclick="clickDossier(this.id)"><i id="i1" class="fa fa-folder-open fa-3x bgVert"></i><br/><p id="p1" class="bgRouge">Dossier1</p></div>
-        		<div id="dos2" class="row bgBleu" onclick="clickDossier(this.id)"><i id="i2" class="fa fa-folder-open fa-3x bgVert"></i><p id="p2" class="bgRouge">Dossier2</p></div>
-        		<div id="dos3" class="row bgBleu" onclick="clickDossier(this.id)"><i id="i3" class="fa fa-folder-open fa-3x bgVert"></i><p id="p3" class="bgRouge">Dossier3</p></div>
-        		<div id="dos4" class="row bgBleu" onclick="clickDossier(this.id)"><i id="i4" class="fa fa-folder-open fa-3x bgVert"></i><p id="p4" class="bgRouge">Dossier4</p></div>
-        		<div id="dos5" class="row bgBleu" onclick="clickDossier(this.id)"><i id="i5" class="fa fa-folder-open fa-3x bgVert"></i><p id="p5" class="bgRouge">Dossier5</p></div>
-        		<div id="dos6" class="row bgBleu" onclick="clickDossier(this.id)"><i id="i6" class="fa fa-folder-open fa-3x bgVert"></i><p id="p6" class="bgRouge">Dossier6</p></div>
-        	</div>
         	<div>
             	<!--boutton retour-->
         	</div>
--- process.php
+++ process.php
@@ -3,32 +3,33 @@
 $info = $_POST['folder'];
 $info();
 
-$fctn = $_POST['fonction'];
-$fctn();
-
 function dossier() {
-    $liste_dossier = shell_exec('ls /home/boul');
+    $liste_dossier = shell_exec('ls /home/omeagazord');
     $tabl_dossier = preg_split('/\s+/', $liste_dossier);
     for ($i=0 ; $i < count($tabl_dossier)-1 ; $i++) {
-        echo "<div class='folder ligne col-md-2'><i class='fa fa-2x fa-folder-o'></i><p>${tabl_dossier[$i]}</p></div>";
+        echo "<div id='" . $tabl_dossier[$i] . "' class='folder ligne col-md-2'onclick='clickDossier(this.id)'><i class='fa fa-2x fa-folder-o'></i><p>${tabl_dossier[$i]}</p></div>";
     }
 }
 
 function testClickDossier()
 {
-    //echo "L'id a été traité par le serveur: " . $_POST['dossier'];
+    //echo $_POST['dossier'];
     
-    $path = "ls /home/" . $_POST['dossier'];
+    $path = "ls /home/omeagazord/" . $_POST['dossier'];
     
-    $output = shell_exec($path);
-    $resultat = preg_split('/\s+/', $output);
+    $liste_dossier = shell_exec($path);
+    $tabl_dossier = preg_split('/\s+/', $liste_dossier);
     
-    for($compteur=0; $compteur< count($resultat); $compteur++)
+    for($i=0; $i< count($tabl_dossier); $i++)
     {
-            echo '<div class="justify-content-center align-items-end">
+            /*echo '<div class="justify-content-center align-items-end">
                     <div class="row col-4 offset-4 fondBleu" onclick="testalert()">
                 <i class="fa fa-folder-open fa-3x"></i><p class="tabulation">'
                 . $resultat[$compteur] . "</p></div></div>";
+        echo $resultat[$compteur];*/
+        
+        
+        echo "<div id='" . $tabl_dossier[$i] . "' class='folder ligne col-md-2'onclick='clickDossier(this.id)'><i class='fa fa-2x fa-folder-o'></i><p>${tabl_dossier[$i]}</p></div>";
     }
 }
 
