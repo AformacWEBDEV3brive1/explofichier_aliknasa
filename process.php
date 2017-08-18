94fa93da0b05e7156d642a8ce6cc3b0dfed6d5ca
omeagazord <omeagazord@omeagazord-HP-ProBook-450-G3>
2017-08-18 11:03:01 +0200

    variable global chemin
    le chemin du dossier home est en dur dans une seule variable globale que
    les autres fonctions utilisent

--- process.php
+++ process.php
@@ -1,15 +1,22 @@
 <?php
 
+$pathTemp = "/home/omeagazord";
 $info = $_POST['folder'];
 $info();
 
 function dossier() {
+    global $pathTemp;
 
-    $liste_dossier = shell_exec('ls /home/boul');
+    $command = "ls " . $pathTemp;
+    
+    $liste_dossier = shell_exec($command);
 
     $tabl_dossier = preg_split('/\s+/', $liste_dossier);
     for ($i = 0; $i < count($tabl_dossier) - 1; $i++) {
-        if (is_dir("/home/boul/" . $tabl_dossier[$i])) {
+        
+        $pathDossier = $pathTemp . "/" . $tabl_dossier[$i];
+        
+        if (is_dir($pathDossier)) {
             echo "<div id='" . $tabl_dossier[$i] . "' class='folder ligne col-md-3'onclick='clickDossier(this.id)'><i class='fa fa-2x fa-folder-o'></i><p>${tabl_dossier[$i]}</p></div>";
         } 
         else {
@@ -19,21 +26,18 @@ function dossier() {
 }
 
 function testClickDossier() {
-    //echo $_POST['dossier'];
+    global $pathTemp;
 
-    $path = "ls /home/boul/" . $_POST['dossier'];
+    $command = "ls " . $pathTemp . '/' . $_POST['dossier'];
 
-    $liste_dossier = shell_exec($path);
+    $liste_dossier = shell_exec($command);
     $tabl_dossier = preg_split('/\s+/', $liste_dossier);
 
     for ($i = 0; $i < count($tabl_dossier)-1; $i++) {
-        /* echo '<div class="justify-content-center align-items-end">
-          <div class="row col-4 offset-4 fondBleu" onclick="testalert()">
-          <i class="fa fa-folder-open fa-3x"></i><p class="tabulation">'
-          . $resultat[$compteur] . "</p></div></div>";
-          echo $resultat[$compteur]; */
-
-        if (is_dir("/home/boul/". $_POST['dossier']. "/" . $tabl_dossier[$i])) {
+        
+        $pathDossier = $pathTemp . '/'. $_POST['dossier']. "/" . $tabl_dossier[$i];
+        
+        if (is_dir($pathDossier)) {
             echo "<div id='" . $tabl_dossier[$i] . "' class='folder ligne col-md-3'onclick='clickDossier(this.id)'><i class='fa fa-2x fa-folder-o'></i><p>${tabl_dossier[$i]}</p></div>";
         } else {
             echo "<div id='" . $tabl_dossier[$i] . "' class='folder ligne col-md-3'onclick='clickDossier(this.id)'><i class='fa fa-2x fa-file-o'></i><p>${tabl_dossier[$i]}</p></div>";
